1d421ddd90dbbd690bf9d01bdc99718ad1754fc3
TaraHsani <147997426+TaraHsani@users.noreply.github.com>
2023-12-03 17:13:25 +0100

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,22 @@
+﻿using CsharpProject.Canstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpProject.Models
+{
+    internal class Product : BaseEntity
+    {
+        public int id { get; set; }
+
+        public string Name { get; set; }
+
+        public string BrandName { get; set; }
+
+        public bool IsValidetion { get; set; }
+
+        public int count { get; set; }
+    }
+}
